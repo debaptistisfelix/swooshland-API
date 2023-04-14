581d8401cfbe516c656d746965f2df5b1d2d574e
Felix De Baptistis <debaptistisfelix@gmail.com>
2023-04-14 19:37:12 +0200

    changed path to utils

--- controllers/factoryHandler.js
+++ controllers/factoryHandler.js
@@ -1,6 +1,7 @@
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
-const APIFeatures = require("./../utils/apiFeatures");
+/* const catchAsync = require("./../utils/catchAsync"); */
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("/../utils/appError");
+const APIFeatures = require("/../utils/apiFeatures");
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
