628805b3886f694e1317277d7491813935881902
Felix De Baptistis <debaptistisfelix@gmail.com>
2023-04-15 11:56:15 +0200

    changed apiFeatures

--- controllers/factoryHandler.js
+++ controllers/factoryHandler.js
@@ -1,10 +1,10 @@
 /* const catchAsync = require("./../utils/catchAsync"); */
 const catchAsync = require("../utils/catchAsync");
 const path = require("path");
-const appError = require("../utils/appError");
-/* const appError = require(path.join(__dirname, "../utils/appError")); */
-const APIFeatures = require("./../utils/apiFeatures");
-/* const APIFeatures = require(path.join(__dirname, "../utils/APIFeatures")); */
+/* const appError = require("../utils/appError"); */
+const appError = require(path.join(__dirname, "../utils/appError"));
+/* const APIFeatures = require("./../utils/apiFeatures"); */
+const APIFeatures = require(path.join(__dirname, "../utils/APIFeatures"));
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
